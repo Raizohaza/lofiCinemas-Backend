5df1ae0c0ef2cc641649eab101f1213496ae7e60
TOÀN PHẠM MINH <toanphamhcmus@gmail.com>
2021-06-07 16:02:58 +0700

    optimal var name, new details router, update profile(user router) + db.sync(www) + err handle(app.js)

--- routes/user.js
+++ routes/user.js
@@ -5,24 +5,26 @@ const User = require('../models/user');
 const EmailCtrl = require('../models/email');
 const randomString = require('random-base64-string');
 const brcypt = require('bcrypt');
+
 router.use(function(req,res,next){
     res.locals.title = 'Log in';
     next();
 })
 //login
-
-
 router.post('/login',asyncHandler (async function(req,res){
     try {
         const {Email,Password} = req.body;
         const found = await User.findbyEmail(Email);
-        if(found && brcypt.compareSync(Password,found.Password)){
+
+        if(found.Verify !== true){
+            res.send(`Check your email to verify your account`);
+        }
+        else if(found  && brcypt.compareSync(Password,found.Password)){
             req.session.UserId = found.id;
+
             if(found.Role == 'admin'){
-                //res.redirect('/admin');
                 res.send({user:found })
             }else{
-                //res.redirect('/home');
                 res.send({user:found })
             }
         }
@@ -40,15 +42,15 @@ router.post('/login',asyncHandler (async function(req,res){
 router.post('/register',asyncHandler (async function(req,res){
     try {
         req.body.Code = null;
-        const {Email,Password,Confirmpassword,Role} = req.body;
-        const finduser = await User.findbyEmail(Email);
-         if(Password != Confirmpassword){
+        const {Email,Password,ConfirmPassword,Role} = req.body;
+        const findUser = await User.findbyEmail(Email);
+         if(Password != ConfirmPassword){
           res.send('Those passwords didn’t match. Try again');
-         }else if(finduser){
+         }else if(findUser){
              res.send('Email da dc su dung');
          }
          
-         else if(!finduser){           
+         else if(!findUser){           
             let Code = randomString(4);
             let hash =  brcypt.hashSync(Password,10);            
             req.body.Password = hash;
@@ -59,12 +61,13 @@ router.post('/register',asyncHandler (async function(req,res){
                 if(host == 'localhost')
                     host = 'http://localhost:5000';
                 let verifyLink = `${host}/user/verify?id=${found.id}&code=${found.Code}`;
-                //const context ="To register click here : localhost:5000/send?code="+found.Code+"&userid="+found.id;
+
                 EmailCtrl.send(found.Email,'Register',verifyLink);
-                res.send(`Check your email to vertify your account`+verifyLink);
-                }else{
-                    res.send('Xác thực mail');
-                }
+                res.send(`Check your email to verify your account ` + verifyLink);
+
+            }else{
+                res.send('Check your email to verify');
+            }
          }   
     } catch (error) {
         res.send(error);
@@ -81,14 +84,14 @@ router.post('/reset',asyncHandler(async function(req, res) {
             const context ="Your password was changed : "+ code;
             EmailCtrl.send(Email,' reset your account ',context);        
     }
-    res.send('Check your email to reset your account'+code);
+    res.send('Check your email to reset your account '+code);
 }));
 
 router.get('/verify',async(req,res,next) =>{
     let id = req.query.id;
     let code = req.query.code;
     console.log('id:',id);
-    //User.findById(id);
+
     let verifyUser = await User.findByPk(id);
     if(verifyUser.Code === code){
         await verifyUser.update(
@@ -100,34 +103,48 @@ router.get('/verify',async(req,res,next) =>{
         });
     }
     else{
-        res.send('Sai code');
+        res.send('Wrong code');
     }
 });
 
-router.post('/profile',asyncHandler(async function(req, res) {
-    const { Name ,Tel,oldpassword,newpassword,confirmpassword} = req.body;
-    console.log({ Name ,Tel,oldpassword,newpassword,confirmpassword});
-    const user = req.user;
+router.post('/:id/profile',asyncHandler(async function(req, res) {
+    const { Name ,Tel,oldPassword,newPassword,confirmPassword} = req.body;
+    //console.log({ Name ,Tel,oldPassword,newPassword,confirmPassword});
+    const uid = req.params.id;  
+    console.log('uid:',uid);
+    const user = await User.findByPk(uid);
     if(Name){
         user.Name = Name;
     }
     if(Tel){
         user.Tel = Tel;
     }
-    if(oldpassword && newpassword && confirmpassword ){
-        if(brcypt.compareSync(oldpassword,user.Password)){
-            if(newpassword == confirmpassword){
-                const hash = brcypt.hashSync(newpassword,10);
+    if(oldPassword && newPassword && confirmPassword ){
+        if(brcypt.compareSync(oldPassword,user.Password)){
+            if(newPassword == confirmPassword){
+                const hash = brcypt.hashSync(newPassword,10);
                 user.Password = hash;
                 user.save();
             }else{
                 res.send('new password != confirm password');
             }
         }else{
-            res.send('your old password is not conrrect');
+            res.send('Your old password is not correct');
         }
     }
     await user.save();
-    res.send('update thanh cong');
+    res.send('Update success!');
+}));
+
+router.post('/:id/details',asyncHandler(async function(req, res) {
+    const uid = req.params.id;  
+    console.log('uid:',uid);
+    const found = await User.findByPk(uid);
+    if(found){
+        res.send({ user : found });
+    }
+    else{
+        res.send('Not found');
+    }
 }));
 module.exports = router;
