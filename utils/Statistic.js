bfe6c87138883433b53ce4f517416c9449f1fe62
acqui8118 <acquy818@gmail.com>
2021-06-22 15:27:48 +0700

    add date

--- utils/Statistic.js
+++ utils/Statistic.js
@@ -1,6 +1,7 @@
 const ShowTime = require('../models/showtime');
 const Booking = require('../models/booking');
 const Cinema = require('../models/cinema');
+const {Op} = require('sequelize');
 async function SumMoney(ShowTimeId){
     const bookings = await Booking.findAll({where: {ShowTimeId},raw: true});
     let Total = 0;
@@ -23,8 +24,22 @@ function groupBy(objectArray, property) {
     }, {})
   }
 
-async function StatisticMovie(MovieId){
-    const showTimes = await ShowTime.findAll({where: {MovieId},raw: true,order:[['DateShow']] });
+async function StatisticMovie(MovieId,startDate,endDate){
+    const showTimes = await ShowTime.findAll({where: {MovieId, DateShow: {[Op.between]:[startDate,endDate]}},raw: true,order:[['DateShow']] });
+    let totalArray = [];
+    let temp = [];
+    for (const showTime of showTimes) {
+        let money = await SumMoney(showTime.id);
+        temp.push({
+            total: money,
+            date: showTime.DateShow
+        });        
+    }
+    temp = groupBy(temp , 'date');
+    return temp;
+};
+async function StatisticCinema(CinemaId,startDate,endDate){
+    const showTimes = await ShowTime.findAll({where: {CinemaId, DateShow: {[Op.between]:[startDate,endDate]}},raw: true,order:[['DateShow']] });
     let totalArray = [];
     let temp = [];
     for (const showTime of showTimes) {
@@ -36,22 +51,9 @@ async function StatisticMovie(MovieId){
     }
       
     temp = groupBy(temp , 'date');
+    console.log(temp);
     return temp;
 };
-async function StatisticCinema(CinemaId){
-    const showTimes = await ShowTime.findAll({where: {CinemaId},raw: true,order:[['DateShow']] });
-    let totalArray = [];
-    let temp = [];
-    for (const showTime of showTimes) {
-        let money = await SumMoney(showTime.id);
-        temp.push({
-            total: money,
-            date: showTime.DateShow
-        });        
-    }
-      
-    temp = groupBy(temp , 'date');
-    return temp;
-};
-StatisticCinema(1);
+StatisticCinema(1,'2000-12-20','2000-12-25');
+
 module.exports ={StatisticMovie,StatisticCinema};
