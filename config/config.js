8264409839b6b760a2848cfe1f76e49db81e7665
acqui8118 <acquy818@gmail.com>
2021-07-14 19:48:58 +0700

    aaa

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports={
-    "facebook_api_key"      :     "4202200626565096",
-    "facebook_api_secret"   :     "12a584fdb46e33ace3acce3768767d1b",
+    "facebook_api_key"      :     "835649640710124",
+    "facebook_api_secret"   :     "fde82364ab3039edf5d05c28fab57fcd",
     "callback_url"          :     "http://localhost:5000/user/auth/facebook/callback",
     "google_clientID"       :     "552583281067-aeqqgkbg4kdutpdfh5venrvanplmhaev.apps.googleusercontent.com",
     "google_clientSecret"   :     "HdJvxpzspQrSj1wXpUJWkC3l",
